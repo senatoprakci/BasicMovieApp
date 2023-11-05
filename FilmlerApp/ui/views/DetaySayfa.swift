a5fc358b671edff63e9eee6408bc62ab110d7f72
Sena Toprakcı <74680647+senatoprakci@users.noreply.github.com>
2023-11-05 22:11:23 +0300

    commit

---
+++ FilmlerApp/ui/views/DetaySayfa.swift
@@ -0,0 +1,32 @@
+//
+//  DetaySayfa.swift
+//  FilmlerApp
+//
+//  Created by Sena Toprakcı on 31.10.2023.
+//
+
+import UIKit
+
+class DetaySayfa: UIViewController {
+
+    @IBOutlet weak var labelFilm: UILabel!
+    @IBOutlet weak var imageViewFilm: UIImageView!
+    @IBOutlet weak var labelFiyat: UILabel!
+    
+    var film:Filmler?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let f = film {
+            labelFilm.text = f.ad
+            imageViewFilm.image = UIImage(named: f.resim!)
+            labelFiyat.text = "\(f.fiyat!) ₺"
+        }
+
+     
+    }
+    
+
+  
+
+}
